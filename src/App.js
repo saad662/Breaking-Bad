50ebb8f99e31ca63d2dd77903c5bd611a2c3037e
saad662 <saadamin662@gmail.com>
2022-09-16 16:27:05 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo.png'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('https://breakingbadapi.com/api/characters')
+      .then(resp => {
+        setItems(resp.data);
+        // console.log(resp.data);
+      }).catch(error => console.error(error));
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const filterItems = items.filter(items =>
+    items.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <div className='center'>
+        <img src={logo} alt='img' />
+      </div>
+
+      <section className='search'>
+        <form>
+          <input
+            type='text'
+            onChange={handleChange}
+            className='form-control'
+            placeholder='Search characters'
+            autoFocus
+          />
+        </form>
+      </section>
+
+      <section className='cards'>
+        {
+          filterItems.map((item) => {
+            return (
+              <div key={item.char_id} className='card'>
+                <div className='card-inner'>
+                  <div className='card-front'>
+                    <img src={item.img} alt='' />
+                  </div>
+                  <div className='card-back'>
+                    <h1>{item.name}</h1>
+                    <ul>
+                      <li>
+                        <strong>Actor Name:</strong> {item.portrayed}
+                      </li>
+                      <li>
+                        <strong>Nickname:</strong> {item.nickname}
+                      </li>
+                      <li>
+                        <strong>Birthday:</strong> {item.birthday}
+                      </li>
+                      <li>
+                        <strong>Status:</strong> {item.status}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </section>
+      
     </div>
   );
 }
-
 export default App;
